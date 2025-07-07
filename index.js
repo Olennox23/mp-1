1428f79140d82341d72e3de833c9b0bd8b0f435d
Owen Lennox <olennox@bu.edu>
2025-07-07 18:35:34 -0400

    finished work

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 
 function addition() {
+    // declare variables
     let first = Number(document.getElementById("first").value);
     let second = Number(document.getElementById("second").value);
     let result = first + second;
+    //if result is negative change text to red
     if (result < 0) {
         output.style.color = 'red';
 
     }
+    //reset to black
     else{
         output.style.color = 'black';
     }
-    document.getElementById("output").innerHTML = String(result);
+    document.getElementById("output").innerHTML = String(result);// print out
 }
 function subtract(){
     let first = Number(document.getElementById("first").value);
@@ -53,18 +56,18 @@ function divide(){
 function exp(){
     let first = Number(document.getElementById("first").value);
     let second = Number(document.getElementById("second").value);
-    let temp = 1;
-    if(second<0){
-        let neg = 1/first
-        let count = second *- 1;
+    let temp = 1; // make temp to hold value
+    if(second<0){ //check if second is neg
+        let neg = 1/first //if neg, you multiply by flipping the number
+        let count = second *- 1; // make a dummy second so that the for loop will work
         for (let i = 0; i < count; i++) {
-            temp *= neg;
+            temp *= neg;//multiply
 
         }
 
     }
     else {
-        for (let i = 0; i < second; i++) {
+        for (let i = 0; i < second; i++) { // multiply first by itself secon times
             temp *= first;
         }
 
@@ -81,12 +84,13 @@ function exp(){
 
 function clearfields(){
 
-    document.getElementById("first").value = "";
-    document.getElementById("second").value = "";
+    document.getElementById("first").value = ""; // make the value in first nothing
+    document.getElementById("second").value = "";//make value in second nothing
 
     // Clear output content and reset color
     let output = document.getElementById("output");
-    output.innerHTML = "";
+    output.innerHTML = ""; //set output to empty
+    
 
 
     }
